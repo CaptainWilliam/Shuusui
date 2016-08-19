aec2857cbfcc56e90bd94b613c70cc5fb84f3f24
William <lhliu1994@gmail.com>
2016-08-19 15:02:11 +0800

    alter some confs

--- info_calculate/info_calculate.sh
+++ info_calculate/info_calculate.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-log_folder='/var/weiboyi/azkaban/MuggleSearchEngine/log/hive_info_calculate_and_upload_to_es/logs'
+log_folder_path='logs'
 log_level='DEBUG'
-working_dir='/var/weiboyi/azkaban/MuggleSearchEngine/log/hive_info_calculate_and_upload_to_es'
-hive_cmd_env='/usr/local/apache-hive-1.2.0/bin/hive'
+# todo: alter XXX
+working_dir='/XXX/'
+hadoop_cmd_env='/XXX/bin/hadoop'
+hive_cmd_env='/XXX/bin/hive'
+hive_conf_path="/XXX/XXX.yaml"
+hive_store_folder_path='/XXX/'
 cd ${working_dir}
 source ./environment_config.sh
-${python_cmd_env} ./main.py $@ --log_folder_path=${log_folder} --log_level=${log_level} --hive_cmd_env=${hive_cmd_env}
+# python_cmd_env = '/usr/local/bin/python'
+${python_cmd_env} ./main.py $@ --log_folder_path=${log_folder_path} --log_level=${log_level} --hadoop_cmd_env=${hadoop_cmd_env} --hive_cmd_env=${hive_cmd_env} --hive_conf_path=${hive_conf_path} --hive_store_folder_path=${hive_store_folder_path}
 if [ $? != 0 ] ; then
     exit 1
 fi
