6f89b9f610fa9f7c173bc9815da232204e07031c
William <lhliu1994@gmail.com>
2016-08-24 09:48:19 +0800

    alter table schema when it changed!

--- hdfs_to_hive/check_table_schema.py
+++ hdfs_to_hive/check_table_schema.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding:utd-8 -*-
+# -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 from utils.hdfs_helper import HDFSHelper
 import commands
@@ -7,7 +7,7 @@ import commands
 __author__ = 'LH Liu'
 
 
-def check_table_schema(hadoop_env_cmd, hive_env_cmd, table_name, schema_path):
+def check_table_schema(hadoop_env_cmd, hive_cmd_env, table_name, database_name, schema_path):
     schema_path = HDFSHelper.read_the_only_file_from_the_path(schema_path)
     status, schema_in_file = commands.getstatusoutput('{} fs -cat {}'.format(hadoop_env_cmd, schema_path))
     if status:
@@ -16,10 +16,12 @@ def check_table_schema(hadoop_env_cmd, hive_env_cmd, table_name, schema_path):
     for line in str(schema_in_file).split('\n'):
         if not (('Warning:'in line) or (line is '')):
             schema_length_in_file += 1
-    status, schema_in_table = commands.getstatusoutput('{} -e "desc {};"'.format(hive_env_cmd, table_name))
+    print schema_length_in_file
+    status, schema = commands.getstatusoutput('{} -e "use {};desc {};"'.format(hive_cmd_env, database_name, table_name))
     if status:
-        raise IOError(schema_in_table)
-    schema_length_in_table = len(str(schema_in_table).split('\n'))
+        raise IOError(schema)
+    schema_length_in_table = len(str(schema).split('\n')) - 7
+    print schema_length_in_table
     if schema_length_in_table == schema_length_in_file:
         return True
     else:
