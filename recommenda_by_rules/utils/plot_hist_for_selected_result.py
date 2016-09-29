ad20fca009ffd6dd166fc2dda50da2c822f87f14
William <lhliu1994@gmail.com>
2016-09-29 15:44:57 +0800

    add a plot util to get the data distribution

---
+++ recommenda_by_rules/utils/plot_hist_for_selected_result.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import matplotlib.pyplot as plt
+import pandas as pd
+
+__author__ = 'LH Liu'
+
+
+def get_data_hist_distribution(data, col=None):
+    if col is not None and isinstance(data, pd.DataFrame):
+        plot_data = data[col]
+    else:
+        plot_data = data
+    plt.hist(plot_data)
+    plt.title('Distribution')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+
+
+def get_data_basic_distribution(x_value, y_value, drawer='o-'):
+    plt.plot(x_value, y_value, drawer)
+    plt.title('Distribution')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
