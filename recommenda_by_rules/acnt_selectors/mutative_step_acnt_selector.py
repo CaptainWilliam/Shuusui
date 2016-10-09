0e2e2de3d110783b50fadbc400c9b644b683087d
William <lhliu1994@gmail.com>
2016-10-09 09:55:28 +0800

    alter the select func

--- recommenda_by_rules/acnt_selectors/mutative_step_acnt_selector.py
+++ recommenda_by_rules/acnt_selectors/mutative_step_acnt_selector.py
@@ -15,8 +15,8 @@ logger = logging.getLogger()
 def mutative_step_acnt_selector(recmd_result, selector_params, company_id):
     tmp_dataframe = check_data_type(recmd_result)
     set_random_seed(company_id)
-    if 'assigned_nums' in selector_params:
-        selected_order = select_with_mutative_step(tmp_dataframe.index, selector_params)
+    if 'assigned_nums' in selector_params and 'assigned_steps' in selector_params:
+        selected_order = select_from_each_segmented_seq(tmp_dataframe.index, selector_params)
     else:
         raise
     # np use (np.col.isin(list)) and python generally use (col in list)
@@ -25,15 +25,35 @@ def mutative_step_acnt_selector(recmd_result, selector_params, company_id):
     return selected_result
 
 
-def select_with_mutative_step(index_seq, selector_params):
-    assigned_nums = selector_params.get('assigned_nums')
+def select_from_each_segmented_seq(index_seq, selector_params):
+    # even spilt the dataset
+    assigned_nums, assigned_steps = selector_params.get('assigned_nums'), selector_params.get('assigned_steps')
     upper_limit = int(len(list(index_seq)))
-    mu_step = int(upper_limit//(len(assigned_nums))+1)
+    mu_step = int(upper_limit // (len(assigned_nums)) + 1)
     segmented_sql_index = range(0, upper_limit, mu_step)
+    # make sure the last one is the last number
     if len(segmented_sql_index) == len(assigned_nums) and segmented_sql_index[-1] != upper_limit:
         segmented_sql_index.append(upper_limit - 1)
+    # pick up in each segmented dataset by mutative steps
     selected_order = []
-    for i in range(0, len(assigned_nums)):
-        # todo: mutative step
-        selected_order += random.sample(index_seq[segmented_sql_index[i]: segmented_sql_index[i+1]], assigned_nums[i])
+    for k in range(0, len(assigned_nums)):
+        selected_order += select_with_mutative_steps(index_seq[segmented_sql_index[k]: segmented_sql_index[k + 1]],
+                                                     int(assigned_nums[k]), assigned_steps[k])
     return selected_order
+
+
+def select_with_mutative_steps(index_seq, assigned_num, assigned_step):
+    if len(index_seq) < assigned_num:
+        raise
+    selected_index = set()
+    selected_time, total_step = 0, 0
+    while len(selected_index) != assigned_num:
+        basic_step = random.randint(0, 20)
+        if len(selected_index) >= assigned_num:
+            total_step = (total_step + assigned_step[-1] * basic_step + index_seq[0]) % index_seq[-1]
+            selected_index.add(index_seq[0] + total_step)
+        else:
+            total_step = (total_step + assigned_step[selected_time] * basic_step + index_seq[0]) % index_seq[-1]
+            selected_index.add(index_seq[0] + total_step)
+        selected_time += 1
+    return list(selected_index)
